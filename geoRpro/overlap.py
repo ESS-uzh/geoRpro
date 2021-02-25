af458e5e26b8c49dd37e90daf0b87191f730a923
sparkvilla <diego.villamaina@gmail.com>
2021-02-25 14:32:04 +0100

    changed array correctiion func

--- geoRpro/overlap.py
+++ geoRpro/overlap.py
@@ -9,7 +9,7 @@ from sklearn.metrics import r2_score
 
 import geoRpro.raster as rst
 
-# * Functions to perform adjacent tiles calibration 
+# * Functions to perform adjacent tiles calibration
 
 import logging
 import pdb
@@ -22,17 +22,17 @@ def calc_diff_arr(arr1, arr2):
     """
     Calculate absolute difference between two arrays. The arrays are min-max
     normalized first.
-    
+
     *************
 
     params
     ____________
 
-    arr1 : nd numpy array 
+    arr1 : nd numpy array
 
-    arr2 : nd numpy array  
+    arr2 : nd numpy array
 
-    
+
     return:
            tuple
            normalizead arrays and thier absolute difference
@@ -71,7 +71,7 @@ def polyfit(x, y, degree=1):
              Degree of the fitting polynomial
 
 
-    return: 
+    return:
            1D numpy atrray
            Coefficients of the model used to fit the x,y data
     """
@@ -96,7 +96,7 @@ def calc_r_squared(model, x, y):
     y : 1D numpy array
         real y values
 
-    
+
     return:
            float
            r squared value
@@ -108,7 +108,7 @@ def calc_r_squared(model, x, y):
 
 def build_a_line(model, x):
     """
-    Compute y values based on x values and model coefficients. It expects a 
+    Compute y values based on x values and model coefficients. It expects a
     linear model, i.e. two coefficients
 
     *************
@@ -144,7 +144,7 @@ def gen_bands(src_1, src_2):
         yield arr1, arr2
 
 
-def array_correction(model, arr_y, arr_x):
+def array_correction(model, target):
     """
     Compute new arr_y values based on a linear model
 
@@ -153,27 +153,26 @@ def array_correction(model, arr_y, arr_x):
     params
     ____________
 
-    
+
     model : 1D numpy array
             (m, b)
- 
-    arr_y : nd numpy array
 
-    arr_x : nd numpy array
+    target : nd numpy array
+
 
     return: nd numpy array
             arr_y_corrected (casted into uint16 dtype (Sent2 dtype))
     """
     # correct arr_y using the model params and arr_x as x
-    arr_y_corr = model[0]*arr_x + model[1]
+    target_corr = model[0]*target + model[1]
     # check whether correction has produced negative values
-    neg_row_idx, neg_col_idx  = np.where(arr_y_corr[0,:,:] < 0)
+    neg_row_idx, neg_col_idx  = np.where(target_corr[0,:,:] < 0)
     if neg_row_idx.size != 0:
         # set negative values to the orifinal arr_y values
         for r, c in zip(neg_row_idx, neg_col_idx):
-            arr_y_corr[0,r,c] = arr_y[0,r,c]
+            target_corr[0,r,c] = target[0,r,c]
 
-    return arr_y_corr.astype('uint16')
+    return target_corr.astype('uint16')
 
 
 
@@ -184,10 +183,10 @@ def bands_correlation(src_ref, src_target, threshold=0.005):
 
     Compute the mean value of tha absolute difference (Reference-Target) for
     each band.
-    If the mean value is higher than the threshold param the Target band 
-    is corrected using array_correction(). 
+    If the mean value is higher than the threshold param the Target band
+    is corrected using array_correction().
     A new mean value (Reference-Target corrected) is computed
-    
+
 
     *************
 
@@ -195,10 +194,10 @@ def bands_correlation(src_ref, src_target, threshold=0.005):
     ____________
 
     src_ref : rasterio.DatasetReader object
-              Reference overlapping polygon 
+              Reference overlapping polygon
 
     src_target : rasterio.DatasetReader object
-                 Target overlapping polygon 
+                 Target overlapping polygon
 
     return : dict
              A mapping of all bands of the Target that need correction
@@ -215,7 +214,7 @@ def bands_correlation(src_ref, src_target, threshold=0.005):
         _, _, diff = calc_diff_arr(arrays[0], arrays[1])
         print(f'diff mean: {round(np.mean(diff), 5)}')
         if round(np.mean(diff), 5) > threshold:
-            array_1_corr = array_correction(model, arrays[1], arrays[0])
+            array_1_corr = array_correction(model, arrays[1])
             _, _, diff_corr = calc_diff_arr(arrays[0], array_1_corr)
             print(f'diff_corr mean: {round(np.mean(diff_corr), 5)}')
             corrections[idx] = model
@@ -225,7 +224,7 @@ def bands_correlation(src_ref, src_target, threshold=0.005):
     return corrections
 
 
-def apply_band_correction(src_ref, src_target, model, band=1):
+def apply_band_correction(src_target, model, band=1):
     """
     Apply a correction to an entire band
 
@@ -233,34 +232,30 @@ def apply_band_correction(src_ref, src_target, model, band=1):
 
     params
     ____________
-    
-    src_ref : rasterio.DatasetReader object
-              Reference raster 
 
     src_target : rasterio.DatasetReader object
-                 Target raster 
+                 Target raster
 
     model : 1D numpy array
             (m, b)
 
     band : int
-           band number 
+           band number
 
     return : nd numpy array
-             target corrected 
+             target corrected
     """
     array_corrected = np.empty((1, src_target.height, src_target.width), dtype=np.int64)
 
-    for idx, w in enumerate(rst.gen_windows(src_ref), start=1):
-        patch_ref, _ = rst.load_window(src_ref, window=w, indexes=[band])
-        patch_target, _ = rst.load_window(src_target, window=w, indexes=[band])
-        patch_target_corrected = array_correction(model, patch_target, patch_ref)
+    for idx, w in enumerate(rst.gen_windows(src_target), start=1):
+        patch_target, _ = rst.load_window(src_target, window=w, bands=[band])
+        patch_target_corrected = array_correction(model, patch_target)
         # fill in the array_corrected with corrected values
         array_corrected[0, w.row_off:w.row_off+w.height, w.col_off:w.col_off+w.width] = patch_target_corrected[0,:,:]
     return array_corrected
 
 
-def apply_correlation_correction(src_ref, src_target, corrections, outdir):
+def apply_correlation_correction(src_target, corrections, outdir):
     """
     Write to disk a corrected Target raster stack. All the bands of src_target stack
     that appear in the corrections param are corrected using the respective model
@@ -270,13 +265,10 @@ def apply_correlation_correction(src_ref, src_target, corrections, outdir):
 
     params
     ____________
-    
-    src_ref : rasterio.DatasetReader object
-              Reference raster 
 
     src_target : rasterio.DatasetReader object
-                 Target raster 
-    
+                 Target raster
+
     corrections: dict
                  band number: model coefficients
 
@@ -289,13 +281,13 @@ def apply_correlation_correction(src_ref, src_target, corrections, outdir):
     print(f'Apply correction to {src_target.files[0]}')
     with ExitStack() as stack_action:
         rstack = rst.Rstack()
-        for index in range(1, src_ref.count+1):
+        for index in range(1, src_target.count+1):
             print(f'Process band: {index}')
             array_target, _ = rst.load_bands(src_target, [index])
             for band, model in corrections.items():
                 if index == band:
                     print(f'Band: {index} will be corrected..')
-                    array_target = apply_band_correction(src_ref, src_target, model, band)
+                    array_target = apply_band_correction(src_target, model, band)
                     break
             band_metadata = src_target.profile
             band_metadata.update({'count': 1})
@@ -303,7 +295,7 @@ def apply_correlation_correction(src_ref, src_target, corrections, outdir):
                 band_metadata))
             rstack.add_item(src_target_corrected)
         rstack.set_metadata_param('interleave', 'band')
-        fname = '_'.join([ntpath.basename(src_target.files[0]).split('.')[0], 'corr']) + '.tif'
+        fname = ''.join([ntpath.basename(src_target.files[0]).split('.')[0], 'Corr']) + '.tif'
         fpath = rst.write_raster(rstack.items, rstack.metadata_collect, os.path.join(outdir, fname))
     return fpath
 
@@ -341,5 +333,5 @@ if __name__ == "__main__":
         band_corrections = bands_correlation(poly_ref, poly_target)
         print(band_corrections)
         if target_fname == 'T20MPB_20200729.tif':
-            apply_correlation_correction(ref, target, band_corrections, stacks_dir)
+            apply_correlation_correction(target, band_corrections, stacks_dir)
 
