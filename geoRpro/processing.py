ff9b8c00c1c2b22b10b1f63b152fcd764d4fd1e5
sparkvilla <diego.villamaina@gmail.com>
2021-10-05 14:45:35 +0200

    bootstrap ComposeBase class and sub-classes

--- geoRpro/processing.py
+++ geoRpro/processing.py
@@ -62,7 +62,8 @@ class ProcessBase:
             ## NOte: Values inserted in data should be checked, property ?!
             self.res = self.pre_process.get('Res')
             self.polygon = self.pre_process.get('Polygon')
-            self.window = tuple(self.pre_process.get('Window'))
+            if self.window:
+                self.window = tuple(self.pre_process.get('Window'))
             self.layer = self.pre_process.get('Layer')
 
     def pre_process_run(self):
@@ -134,7 +135,72 @@ class ProcessBase:
 
 
 class ComposeBase:
-    pass
+    INSTRUCTIONS = {'Input', 'Data_Dir', 'Satellite', 'Sat_Dir'}
+
+    def __init__(self, instructions):
+        self.instructions = copy.deepcopy(instructions)
+        self.input = {}
+        self.output = {}
+        self._parse_instructions()
+        self._get_fpaths()
+        self.metadata = None
+
+    def _parse_instructions(self):
+
+        for k, _ in self.instructions.items():
+
+            if k not in self.INSTRUCTIONS:
+                raise ValueError(f'{k} is not a valid argument')
+
+            ## NOte: Values inserted in data should be checked, property ?!
+            self.input = self.instructions.get('Input')
+            self.name = list(self.input.keys())[0]
+            self.data_dir = self.instructions.get('Data_Dir')
+            self.satellite = self.instructions.get('Satellite')
+            self.sat_dir = self.instructions.get('Sat_Dir')
+
+
+    def _get_fpaths(self):
+        # mapping band with fpath using Sentinel2
+        if self.satellite == 'Sentinel2' and self.sat_dir:
+            s10 = Sentinel2(os.path.join(self.sat_dir, 'R10m'))
+            s20 = Sentinel2(os.path.join(self.sat_dir, 'R20m'))
+            s60 = Sentinel2(os.path.join(self.sat_dir, 'R60m'))
+
+            for idx, band_name in enumerate(self.input[self.name]):
+                try:
+                    self.input[self.name][idx] = s10.get_fpath(band_name)
+                except KeyError:
+                    try:
+                        self.input[self.name][idx] = s20.get_fpath(band_name)
+                    except KeyError:
+                        try:
+                            self.input[self.name][idx] = s60.get_fpath(band_name)
+                        except KeyError:
+                            raise ValueError(f"Cannot find band: '{band_name}'. Please \
+                                    provide valid Sentinel2 band name.")
+        # mapping band with fpath using fname
+        elif self.data_dir:
+            for idx, file_name in enumerate(self.input[self.name]):
+                self.input[self.name][idx] = os.path.join(self.data_dir, file_name)
+
+
+    def _check_metadata(self):
+        if self.output:
+            test_src = self.output[self.name][0]
+            end_srcs = self.output[self.name][1:]
+        for src in end_srcs:
+            if src.crs.to_epsg() != test_src.crs.to_epsg():
+                raise ValueError('Raster data must have the same CRS')
+            if src.width != test_src.width or src.height != test_src.height:
+                raise ValueError('Raster data must have the same size')
+            if src.res != test_src.res:
+                raise ValueError('Raster data must have the same spacial resolution')
+            self.metadata = test_src.profile
+            self.metadata.update(driver='GTiff')
+
+    def run(self):
+        pass
 
 
 class RProcess(ProcessBase):
@@ -208,6 +274,45 @@ class RReplace(ProcessBase):
                 io.write_array_as_raster(arr_replaced, meta, fpath)
                 self.outputs[name] = fpath
 
+class RStack(ComposeBase):
+
+    def __init__(self, instructions):
+        super().__init__(instructions)
+
+    def run(self):
+        print(f'Start stack procedure for {self.input[self.name]}')
+        with ExitStack() as stack_files:
+
+            srcs = [stack_files.enter_context(rasterio.open(v))
+                    for v in self.input[self.name]]
+            self.output = {self.name: srcs}
+            self._check_metadata()
+            self.metadata.update(count=len(self.output[self.name]))
+            fpath = os.path.join(self.data_dir, self.name + '.tif')
+            io.write_raster(srcs, self.metadata, fpath)
+            self.output[self.name] = fpath
+        print('Done stacking...')
+
+class RNdvi(ComposeBase):
+
+    def __init__(self, instructions):
+        super().__init__(instructions)
+
+    def run(self):
+        print(f'Calculate ndvi index for {self.input[self.name]}')
+        with ExitStack() as stack_files:
+
+            srcs = [stack_files.enter_context(rasterio.open(v))
+                    for v in self.input[self.name]]
+            self.output = {self.name: srcs}
+            self._check_metadata()
+            index = rst.Indexes(self.metadata)
+            arr, meta = index.ndvi(srcs[0], srcs[1])
+            fpath = os.path.join(self.data_dir, self.name + '.tif')
+            io.write_array_as_raster(arr, meta, fpath)
+            self.output[self.name] = fpath
+        print('Done calculating ndvi...')
+
 if __name__ == '__main__':
 
     ORIGIN = '/home/diego/work/dev/data/test_data/S2A_MSIL2A_20190906T073611_N0213_R092_T37MBN_20190906T110000.SAFE/GRANULE/L2A_T37MBN_A021968_20190906T075543/IMG_DATA'
@@ -245,94 +350,6 @@ if __name__ == '__main__':
     scl_mask_arr, meta_scl_mask = rst.load(scl_mask)
     b11, meta_b11 = rst.load(b11_replaced)
 
-#class RStack(ComposeBase):
-#    ALLOWED = ('Inputs', 'Replace')
-#
-#    def __init__(self, instructions, data_dir, satellite=None, sat_dir=None,
-#            metadata=None):
-#        super().__init__(instructions, data_dir, satellite, sat_dir)
-#        self.metadata = metadata
-#
-#    def _parse_instructions(self):
-#
-#        for k, _ in self.instructions.items():
-#
-#            if k not in self.ALLOWED:
-#                raise ValueError(f'{k} is not a valid argument')
-#
-#            ## NOte: Values inserted in data should be checked, property ?!
-#            self.inputs = self.instructions.get('Inputs')
-#            self.replace_val = self.instructions.get('Replace')
-#
-#    def _get_fpaths(self):
-#        # mapping band with fpath using Sentinel2
-#        if self.satellite == 'Sentinel2' and self.sat_dir:
-#            s10 = Sentinel2(os.path.join(self.sat_dir, 'R10m'))
-#            s20 = Sentinel2(os.path.join(self.sat_dir, 'R20m'))
-#            s60 = Sentinel2(os.path.join(self.sat_dir, 'R60m'))
-#
-#            for name, bands in self.inputs.items():
-#                for idx, band_name in enumerate(bands):
-#                    try:
-#                        self.inputs[name][idx] = s10.get_fpath(band_name)
-#                    except KeyError:
-#                        try:
-#                            self.inputs[name][idx] = s20.get_fpath(band_name)
-#                        except KeyError:
-#                            try:
-#                                self.inputs[name][idx] = s60.get_fpath(band_name)
-#                            except KeyError:
-#                                raise ValueError(f"Cannot find band: '{band_name}'. Please \
-#                                        provide valid Sentinel2 band name.")
-#
-#        # mapping band with fpath using fname
-#        elif self.data_dir:
-#            for name, file_names in self.inputs.items():
-#                for idx, file_name in enumerate(bands):
-#                    self.inputs[name][idx] = os.path.join(self.data_dir, file_name)
-#
-#
-#    def _check_metadata(self):
-#        for name, srcs in self.outputs.items():
-#            test_scr = srcs[0]
-#            for idx, src in srcs[1:]:
-#                if src.crs.to_epsg() != test_src.crs.to_epsg():
-#                    raise ValueError('Raster data must have the same CRS')
-#                if src.width != test_src.width or src.heigth != test_src.heigth:
-#                    raise ValueError('Raster data must have the same size')
-#                if src.res != test_src.res:
-#                    raise ValueError('Raster data must have the same spacial resolution')
-#        self.metadata = self.outputs[test_name].profile
-#        self.metadata.update(count=len(self.outputs.keys()))
-#        self.metadata.update(driver='GTiff')
-#
-#    def run(self):
-#
-#        srcs = []
-#        with ExitStack() as stack_files:
-#            for name, fpaths in self.inputs.items():
-#                for idx, fpath in enumerate(fpaths):
-#                    self.outputs[name][idx] = rasterio.open(fpath)
-#
-#            self._check_metadata()
-#
-#            with ExitStack() as stack_action:
-#
-#                for name, src in self.outputs.items():
-#
-#                    if self.replace_val:
-#                        print(f"Replace values at mask array position")
-#                        # check that mask and array are the same dimension
-#                        arr, meta = rst.load(src)
-#                        with rasterio.open(self.replace_val[0]) as src_mask:
-#                            mask, _ = rst.load(src_mask)
-#                        assert mask.shape == arr.shape, 'Array and mask must the have same shape'
-#                        arr = rst.apply_mask(arr, mask, fill_value=self.replace_val[1])
-#                        src = stack_action.enter_context(io.to_src(arr, meta))
-#                    srcs.append(src)
-#                fpath = os.path.join(self.data_dir, name + '.tif')
-#                io.write_raster(srcs, metadata, fpath)
-#                self.outputs[name] = fpath
 #
 #class RIndex(Task):
 #    pass
